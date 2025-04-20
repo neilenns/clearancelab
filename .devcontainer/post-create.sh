3f52ddc64bf9c47b8f4718b0249654b0bee7542d
Neil Enns <neile@live.com>
2025-04-20 09:45:24 -0700

    Fix devcontainer (#61)
    * Fix initial devcontainer setup
    
    * More fixes
    
    * Remove API key
    
    * More cleanup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,5 +3,16 @@
 set -euo pipefail
 
 . "${NVM_DIR}/nvm.sh" && nvm install
-pnpm config set store-dir ../.pnpm-store
-COREPACK_ENABLE_DOWNLOAD_PROMPT=0  pnpm install turbo --global && pnpm install
+
+export PNPM_HOME="${HOME}/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+
+grep -qxF 'export PNPM_HOME="$HOME/.local/share/pnpm"' ~/.zshrc || \
+  echo 'export PNPM_HOME="$HOME/.local/share/pnpm"' >> ~/.zshrc
+grep -qxF 'export PATH="$PNPM_HOME:$PATH"' ~/.zshrc || \
+  echo 'export PATH="$PNPM_HOME:$PATH"' >> ~/.zshrc
+
+pnpm config set store-dir /workspaces/clearancelab/.pnpm-store
+
+COREPACK_ENABLE_DOWNLOAD_PROMPT=0 pnpm add -g turbo
+pnpm install
