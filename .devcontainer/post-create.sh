ed32ee3657541d942c3a6fe1b25439b5cb4aabf0
Neil Enns <neile@live.com>
2025-04-20 14:17:30 +0000

    Fix more stuff

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-. ${NVM_DIR}/nvm.sh && nvm install
+set -euo pipefail
+
+. "${NVM_DIR}/nvm.sh" && nvm install
 pnpm config set store-dir ../.pnpm-store
 COREPACK_ENABLE_DOWNLOAD_PROMPT=0 pnpm install
