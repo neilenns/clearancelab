f557631d01fa515fa72888bf4d03431de6593efc
Neil Enns <neile@live.com>
2025-04-21 04:21:04 -0700

    Verify Docker image rebuilds if the API code changes (#89)
    * Verify Docker image rebuilds if the API code changes
    Fixes #87
    
    * Fix CI
    
    * More fixes to dockerfile and ci
    
    * Clean up local run commands
    
    * Clean up
    
    * Fix CI
    
    * CI change again
    
    * Try again
    
    * Hmm
    
    * Hmmmmm
    
    * Oops
    
    * More CI improvements
    
    * More CI tweaks
    
    * One more time
    
    * Try fixing turboaffected

--- apps/api/src/routes/default.ts
+++ apps/api/src/routes/default.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", function (_req, res) {
-  res.json({ status: "success" });
+  res.json({ status: "ok" });
 });
 
 export default router;
