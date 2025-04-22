6824126ea286f80b11f6d6c38c66ea0c29840572
Neil Enns <neile@live.com>
2025-04-22 12:45:14 +0000

    Try it

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -12,13 +12,13 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export PNPM_HOME="${HOME}/.local/share/pnpm"
 export PATH="$PNPM_HOME:$PATH"
+pnpm config set store-dir "$PNPM_STORE" # This comes from devcontainer.json, and is mounted as a volume in docker-compose.yaml
 
 grep -qxF 'export PNPM_HOME="$HOME/.local/share/pnpm"' ~/.zshrc || \
   echo 'export PNPM_HOME="$HOME/.local/share/pnpm"' >> ~/.zshrc
 grep -qxF 'export PATH="$PNPM_HOME:$PATH"' ~/.zshrc || \
   echo 'export PATH="$PNPM_HOME:$PATH"' >> ~/.zshrc
 
-pnpm config set store-dir "$PWD/.pnpm-store"
 
 COREPACK_ENABLE_DOWNLOAD_PROMPT=0 pnpm add -g turbo
 pnpm install
