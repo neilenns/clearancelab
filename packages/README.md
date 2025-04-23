85594fcf9505eca84cf9bf83412dfdbdc986c2e8
Neil Enns <neile@live.com>
2025-04-23 11:44:50 +0000

    Sheesh

---
+++ packages/README.md
@@ -0,0 +1,5 @@
+# Packages for the monorepo
+
+The method for building packages and managing shared configs is primarily based on the [mono-ts example](https://github.com/0x80/mono-ts). Typescript configs are based on the [typescript-config](https://github.com/0x80/typescript-config) example.
+
+A summary is available in the [companion blog post](https://thijs-koerselman.medium.com/my-quest-for-the-perfect-ts-monorepo-62653d3047eb).
---
+++ packages/validators/tsup.config.ts
@@ -0,0 +1,17 @@
+// This comes from https://github.com/0x80/mono-ts/blob/main/packages/common/tsup.config.ts
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: {
+    index: "src/index.ts",
+  },
+  format: ["esm"],
+  target: "es2022",
+  sourcemap: true,
+
+  /**
+   * Do not use tsup for generating d.ts files because it can not generate type
+   * the definition maps required for go-to-definition to work in our IDE. We
+   * use tsc for that.
+   */
+});
