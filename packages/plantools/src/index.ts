057fc0db8e39582964979a4290e4a875bce99b2f
Neil Enns <neile@live.com>
2025-04-23 11:40:31 +0000

    Man what a lot of work

--- packages/plantools/src/index.ts
+++ packages/plantools/src/index.ts
@@ -1,7 +1,1 @@
-export function isValidRoute(route: string): boolean {
-  return /^[A-Z]{3,5}$/.test(route); // e.g., basic check for ICAO codes
-}
-
-export function isValidCallsign(callsign: string): boolean {
-  return /^[A-Za-z]{3}\d{1,4}$/.test(callsign);
-}
+export * from "./validators";
