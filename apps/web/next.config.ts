652cd135cea6950843c45ccb6fe811738dce3982
Neil Enns <info@neilenns.com>
2025-04-30 20:01:54 +0000

    Fix next config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,9 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-require-imports
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+import type { NextConfig } from "next";
 
-const nextConfig = {};
+const nextConfig: NextConfig = {};
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-module.exports = withBundleAnalyzer(nextConfig);
+export default nextConfig;
