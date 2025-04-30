36605e6706b5d89928516ef9cd9a76154bb206dc
Neil Enns <info@neilenns.com>
2025-04-30 23:46:19 +0000

    Revert "Fix nextjs caching (#248)"
    This reverts commit 926110fe4332d5a26bc720d5eb54fd80df51451f.

--- apps/web/open-next.config.ts
+++ apps/web/open-next.config.ts
@@ -1,11 +1,11 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
 
 // This method of caching fetches comes from https://opennext.js.org/cloudflare/caching.
 export default defineCloudflareConfig({
-  incrementalCache: kvIncrementalCache,
+  incrementalCache: r2IncrementalCache,
   queue: doQueue,
   tagCache: d1NextTagCache,
 });
