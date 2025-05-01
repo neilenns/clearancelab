13e57390abf90a3130e99d8135ca9f9df0da6e60
Neil Enns <neile@live.com>
2025-04-30 19:51:26 -0700

    Configure opennextjs for local dev (#254)
    Fixes #162

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,3 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {};
 
 export default nextConfig;
+
+// This enables local dev access to Cloudflare bindings. It comes from
+// https://opennext.js.org/cloudflare/get-started#11-develop-locally
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
+void initOpenNextCloudflareForDev();
