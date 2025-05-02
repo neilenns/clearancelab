471c5fca69a4548f2fb81e573d46ac2ca6569c72
Neil Enns <neile@live.com>
2025-05-02 12:33:32 -0700

    Fix a bunch of stuff (#293)

--- .devcontainer/seed/init.sh
+++ .devcontainer/seed/init.sh
@@ -3,15 +3,14 @@ set -euo pipefail
 
 # Initialize the database
 
-# All paths need to be made absolute since it runs in the context of a devcontainer.
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export DB_URI="mongodb://db:27017/clearancelab"
 
 echo "👤 Dropping database, creating user and disabling telemetry..."
-mongosh ${DB_URI} "${SCRIPT_DIR}/init.js" # Basic user setup and disable telemetry
+mongosh "${DB_URI}" "${SCRIPT_DIR}/init.js" > /dev/null
 
 echo "📦 Importing data..."
-mongoimport --uri=${DB_URI} --collection=scenarios --file="${SCRIPT_DIR}/clearancelab.scenarios.json" --jsonArray
-mongoimport --uri=${DB_URI} --collection=airportinfo --file="${SCRIPT_DIR}/clearancelab.airportinfo.json" --jsonArray
-mongoimport --uri=${DB_URI} --collection=vatsimflightplans --file="${SCRIPT_DIR}/clearancelab.vatsimflightplans.json" --jsonArray
-mongoimport --uri=${DB_URI} --collection=auth0users --file="${SCRIPT_DIR}/clearancelab.auth0users.json" --jsonArray
+mongoimport --uri="${DB_URI}" --collection=scenarios --file="${SCRIPT_DIR}/clearancelab.scenarios.json" --jsonArray > /dev/null
+mongoimport --uri="${DB_URI}" --collection=airportinfo --file="${SCRIPT_DIR}/clearancelab.airportinfo.json" --jsonArray > /dev/null
+mongoimport --uri="${DB_URI}" --collection=vatsimflightplans --file="${SCRIPT_DIR}/clearancelab.vatsimflightplans.json" --jsonArray > /dev/null
+mongoimport --uri="${DB_URI}" --collection=auth0users --file="${SCRIPT_DIR}/clearancelab.auth0users.json" --jsonArray > /dev/null
