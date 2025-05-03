dbcf19fe6df4010b58f5a00dcdf5aa96a0ebddf7
Neil Enns <info@neilenns.com>
2025-05-03 11:25:59 +0000

    Add an info icon for the controller

--- apps/web/src/components/chat.tsx
+++ apps/web/src/components/chat.tsx
@@ -2,12 +2,16 @@
 
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utilities";
+import { Info } from "lucide-react";
 import React from "react";
+import { Button } from "./ui/button";
 
 export interface ChatMessage {
   from: "controller" | "pilot";
   content: string | React.ReactNode;
+  onInfoClick?: () => void;
 }
+
 export interface ChatProperties {
   messages: ChatMessage[];
   pilotCallsign?: string;
@@ -20,19 +24,42 @@ export function Chat({ messages }: ChatProperties) {
       <Card>
         <CardContent>
           <div className="space-y-4">
-            {messages.map((message, index) => (
-              <div
-                key={index}
-                className={cn(
-                  "flex w-max max-w-[75%] flex-col gap-2 rounded-lg px-2 py-2 text-sm",
-                  message.from === "controller"
-                    ? "ml-auto bg-primary text-primary-foreground"
-                    : "bg-muted",
-                )}
-              >
-                {message.content}
-              </div>
-            ))}
+            {messages.map((message, index) => {
+              const isController = message.from === "controller";
+
+              return (
+                <div
+                  key={index}
+                  className={cn(
+                    "flex items-start gap-2",
+                    isController ? "justify-end" : "justify-start",
+                  )}
+                >
+                  {isController && message.onInfoClick && (
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      onClick={message.onInfoClick}
+                    >
+                      <Info
+                        className="self-center"
+                        aria-label="Controller info"
+                      />
+                    </Button>
+                  )}
+                  <div
+                    className={cn(
+                      "w-max max-w-[75%] flex-col gap-2 rounded-lg px-3 py-2 text-sm",
+                      isController
+                        ? "bg-primary text-primary-foreground"
+                        : "bg-muted",
+                    )}
+                  >
+                    {message.content}
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </CardContent>
       </Card>
