f21576b8d62c1f8ccb5d73e2503502c2dba3d8f0
Neil Enns <info@neilenns.com>
2025-05-03 19:25:27 +0000

    Add an edit button for logged-in people

--- apps/web/src/app/lab/[id]/client-section.tsx
+++ apps/web/src/app/lab/[id]/client-section.tsx
@@ -2,15 +2,26 @@
 
 import { Answer } from "@/components/answer";
 import FPE from "@/components/fpe/fpe";
+import { Button } from "@/components/ui/button";
 import { Scenario } from "@workspace/validators";
+import Link from "next/link";
 
 interface ClientSectionProperties {
   scenario: Scenario;
+  canEdit: boolean;
 }
 
-export default function ClientSection({ scenario }: ClientSectionProperties) {
+export default function ClientSection({
+  scenario,
+  canEdit,
+}: ClientSectionProperties) {
   return (
     <main className="p-6 overflow-y-auto">
+      {canEdit && (
+        <Button asChild>
+          <Link href={`/admin/scenarios/edit/${scenario._id}`}>Edit</Link>
+        </Button>
+      )}
       <FPE scenario={scenario} />
       <Answer scenario={scenario} />
     </main>
