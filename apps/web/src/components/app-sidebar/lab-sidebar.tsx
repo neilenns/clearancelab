fe6f030ecc0099fba0fd47b0fee1c4c3f8d6dd6e
Neil Enns <info@neilenns.com>
2025-05-03 01:07:35 +0000

    Add a "surprise me!" button Fixes #301

--- apps/web/src/components/app-sidebar/lab-sidebar.tsx
+++ apps/web/src/components/app-sidebar/lab-sidebar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
 import {
   Sidebar,
   SidebarContent,
@@ -11,23 +12,38 @@ import {
   SidebarMenu,
 } from "@/components/ui/sidebar";
 import { Scenario } from "@workspace/validators";
-import { useParams } from "next/navigation";
+import { WandSparkles } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 import { ScenarioItem } from "./scenario-item";
 
 // Extend the props from the base Sidebar and add scenarios
-interface AppSidebarProperties extends React.ComponentProps<typeof Sidebar> {
+interface LabSidebarProperties extends React.ComponentProps<typeof Sidebar> {
   scenarios: Scenario[];
 }
 
-export function AppSidebar({ scenarios, ...properties }: AppSidebarProperties) {
+export function LabSidebar({ scenarios, ...properties }: LabSidebarProperties) {
   const parameters = useParams();
   const selectedId = parameters.id as string;
+  const router = useRouter();
+
+  const onClickHandler = () => {
+    const randomIndex = Math.floor(Math.random() * scenarios.length);
+    const randomScenario = scenarios[randomIndex];
+
+    if (randomScenario._id) {
+      router.replace(`/lab/${randomScenario._id}`);
+    }
+  };
 
   return (
     <aside>
       <Sidebar {...properties}>
         <SidebarHeader>
           <h3>Scenarios</h3>
+          <Button onClick={onClickHandler}>
+            <WandSparkles />
+            Surprise me!
+          </Button>
         </SidebarHeader>
         <SidebarContent>
           <SidebarGroup>
