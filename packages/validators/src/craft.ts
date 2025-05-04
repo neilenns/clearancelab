43f8cd3b32574cdf43abf677aa2f037e21f08c61
Neil Enns <info@neilenns.com>
2025-05-04 12:00:53 +0000

    Frequency works ok I guess

--- packages/validators/src/craft.ts
+++ packages/validators/src/craft.ts
@@ -4,7 +4,7 @@ export const CraftSchema = z.object({
   altitude: z.string().optional(),
   clearanceLimit: z.string().optional(),
   controllerName: z.string().optional(),
-  frequency: z.string().optional(),
+  frequency: z.coerce.number().optional(),
   route: z.string().optional(),
   telephony: z.string().optional(),
 });
