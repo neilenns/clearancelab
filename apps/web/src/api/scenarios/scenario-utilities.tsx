9e0dc64adf31bd2caf9b71055151d088384723f1
Neil Enns <info@neilenns.com>
2025-05-05 23:04:36 +0000

    Add code comment

--- apps/web/src/api/scenarios/scenario-utilities.tsx
+++ apps/web/src/api/scenarios/scenario-utilities.tsx
@@ -34,6 +34,9 @@ export function transformFormData(payload: FormData): TransformResult {
   }
 
   const formData = unflatten(Object.fromEntries(payload));
+
+  // Handle the case where formData didn't include these properties For example,
+  // when canClear is false, craft isn't included.
   formData.airportConditions ??= {};
   formData.plan ??= {};
   formData.craft ??= {};
