92a6f2bbf116a426eaa055afcaeb0b6492c6d150
Neil Enns <info@neilenns.com>
2025-05-07 02:15:54 +0000

    Fixed the popover!!!

--- apps/web/src/components/data-table/filters/boolean-filter.tsx
+++ apps/web/src/components/data-table/filters/boolean-filter.tsx
@@ -5,6 +5,7 @@ import {
 } from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
 import { FunnelIcon } from "lucide-react";
+import { useState } from "react";
 import { FilterProperties } from "./filter";
 
 const booleanFilterOptions = [
@@ -14,13 +15,14 @@ const booleanFilterOptions = [
 ];
 
 export const BooleanFilter = ({ column }: FilterProperties) => {
+  const [isPopoverOpen, setPopoverOpen] = useState(false);
   const columnFilterValue = column.getFilterValue();
 
   return (
     <div className="flex items-center justify-center space-x-1">
-      <Popover>
+      <Popover open={isPopoverOpen} onOpenChange={setPopoverOpen}>
         <PopoverTrigger asChild>
-          <button>
+          <button onClick={() => setPopoverOpen((previous) => !previous)}>
             <FunnelIcon
               className={cn(
                 "h-4 w-4",
@@ -38,6 +40,7 @@ export const BooleanFilter = ({ column }: FilterProperties) => {
               role="menuitem"
               onClick={() => {
                 column.setFilterValue(option.value);
+                setPopoverOpen(false); // Close the popover
               }}
               aria-selected={columnFilterValue === option.value}
               className={`w-full text-left px-2 py-1 rounded hover:bg-muted ${
