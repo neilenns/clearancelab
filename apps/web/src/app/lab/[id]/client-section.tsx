f0cced4f684fcf69f180dee6e72b1ec4e9711aec
Neil Enns <info@neilenns.com>
2025-05-08 00:15:57 +0000

    More role stuff, but it's broken DISABLE_AUTH

--- apps/web/src/app/lab/[id]/client-section.tsx
+++ apps/web/src/app/lab/[id]/client-section.tsx
@@ -5,24 +5,42 @@ import { ConfirmDeleteDialog } from "@/components/confirm-delete-dialog";
 import FPE from "@/components/fpe/fpe";
 import { Button } from "@/components/ui/button";
 import { useDeleteScenario } from "@/hooks/use-delete-scenario";
+import { useUser } from "@auth0/nextjs-auth0";
 import { Scenario } from "@workspace/validators";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 interface ClientSectionProperties {
   scenario: Scenario;
-  canEdit: boolean;
 }
 
-export default function ClientSection({
-  scenario,
-  canEdit,
-}: ClientSectionProperties) {
+export default function ClientSection({ scenario }: ClientSectionProperties) {
   const deleteScenario = useDeleteScenario();
   const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
   const router = useRouter();
+  const { user, isLoading, error } = useUser();
+  const [canEdit, setCanEdit] = useState(false);
+  const [canDelete, setCanDelete] = useState(false);
+
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+
+    if (error || !user) {
+      console.error("Error fetching user:", error);
+      setCanEdit(false);
+      setCanDelete(false);
+      return;
+    }
+
+    setCanEdit(user.permissions.includes("edit:scenarios"));
+    setCanDelete(user.permissions.includes("delete:scenarios"));
+
+    console.log("User:", user);
+  }, [user, isLoading, error]);
 
   const deleteScenarioHandler = async () => {
     toast.promise(deleteScenario(scenario._id), {
@@ -39,24 +57,29 @@ export default function ClientSection({
 
   return (
     <main className="p-6 overflow-y-auto">
-      {canEdit && (
-        <div className="space-x-2">
+      <div className="space-x-2 mb-4">
+        {canEdit && (
           <Button asChild>
             <Link href={`/admin/scenarios/edit/${scenario._id}`}>Edit</Link>
           </Button>
-          <Button
-            variant="destructive"
-            onClick={() => setIsDeleteDialogOpen(true)}
-          >
-            Delete
-          </Button>
-          <ConfirmDeleteDialog
-            onConfirm={deleteScenarioHandler}
-            isDialogOpen={isDeleteDialogOpen}
-            setIsDialogOpen={setIsDeleteDialogOpen}
-          />
-        </div>
-      )}
+        )}
+        {canDelete && (
+          <>
+            <Button
+              variant="destructive"
+              onClick={() => setIsDeleteDialogOpen(true)}
+            >
+              Delete
+            </Button>
+            <ConfirmDeleteDialog
+              onConfirm={deleteScenarioHandler}
+              isDialogOpen={isDeleteDialogOpen}
+              setIsDialogOpen={setIsDeleteDialogOpen}
+            />
+          </>
+        )}
+      </div>
+
       <FPE scenario={scenario} />
       <Answer scenario={scenario} />
     </main>
