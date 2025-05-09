e2f453d53ccfcd5e85332751ba293759ab2da7ea
Neil Enns <info@neilenns.com>
2025-05-09 17:37:05 +0000

    Add suspense

--- apps/web/src/app/admin/scenarios/page.tsx
+++ apps/web/src/app/admin/scenarios/page.tsx
@@ -1,7 +1,9 @@
 "use server";
 import { fetchScenariosSummary } from "@/api/scenarios/fetch-scenarios";
 import { AdminHeader } from "@/components/admin-header";
+import { Suspense } from "react";
 import ClientSection from "./client-section";
+import Loading from "./loading";
 
 export default async function Page() {
   const result = await fetchScenariosSummary();
@@ -10,12 +12,14 @@ export default async function Page() {
   return (
     <div>
       <AdminHeader />
-      <main
-        className="flex h-full flex-col justify-center px-4 py-4"
-        aria-label="Scenarios management"
-      >
-        <ClientSection scenarios={scenarios} />
-      </main>
+      <Suspense fallback={<Loading />}>
+        <main
+          className="flex h-full flex-col justify-center px-4 py-4"
+          aria-label="Scenarios management"
+        >
+          <ClientSection scenarios={scenarios} />
+        </main>
+      </Suspense>
     </div>
   );
 }
