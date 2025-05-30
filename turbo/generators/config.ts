f8eb4ba40c9abf102d289fef4c5b03f8bb80be13
Neil Enns <neile@live.com>
2025-05-30 09:28:13 -0700

    Remove package generator template (#512)
    Fixes #150

--- turbo/generators/config.ts
+++
@@ -1,6 +0,0 @@
-import { PlopTypes } from "@turbo/gen";
-import generatePackage from "./templates/generate-package";
-
-export default function (plop: PlopTypes.NodePlopAPI) {
-  generatePackage(plop);
-}
