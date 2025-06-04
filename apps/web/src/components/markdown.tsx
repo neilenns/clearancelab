9452b8ab6ed486f8416652999ba3594e504b9fa7
Neil Enns <info@neilenns.com>
2025-06-04 18:57:11 +0000

    Add markdown support to explanations Fixes #573

---
+++ apps/web/src/components/markdown.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+interface MarkdownProperties {
+  children: string;
+}
+
+const Markdown = ({ children }: MarkdownProperties) => {
+  return (
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      components={{
+        a: ({ href, children }) => (
+          <Link
+            className="text-[var(--color-primary)] underline"
+            href={href ?? "#"}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            {children}
+          </Link>
+        ),
+      }}
+    >
+      {children}
+    </ReactMarkdown>
+  );
+};
+
+export default Markdown;
