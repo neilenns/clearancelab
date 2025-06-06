cf53c36c29406f999ddfdfcd33fc09935c608eb4
Neil Enns <info@neilenns.com>
2025-06-06 12:57:10 +0000

    More work

--- packages/vatsim/src/flight-plan.ts
+++ packages/vatsim/src/flight-plan.ts
@@ -1,6 +1,17 @@
+import {
+  convertToNumber,
+  getRandomAirportCode,
+  getRandomCid,
+  getRandomExternalBcn,
+  getRandomName,
+  getRandomVatsimId,
+} from "@workspace/plantools";
+import { Scenario } from "@workspace/validators";
 import { VatsimPilot } from "./types";
 import { VatsimPrefile } from "./types/vatsim-prefile";
 import { getVatsimData } from "./vatsim";
+
+type VatsimFlightPlan = VatsimPilot | VatsimPrefile | undefined;
 
 /**
  * Retrieves a flight plan from VATSIM. Returns undefined if the flight isn't found.
@@ -9,33 +20,57 @@ import { getVatsimData } from "./vatsim";
  */
 export async function getFlightPlan(
   callsign: string,
-): Promise<VatsimPilot | VatsimPrefile | undefined> {
+): Promise<VatsimFlightPlan> {
   try {
     const vatsimData = await getVatsimData();
+    let result: VatsimFlightPlan;
 
     // Try and find a prefile first.
-    const prefile = vatsimData.prefiles.find(
+    result = vatsimData.prefiles.find(
       (prefile) => prefile.callsign === callsign,
     );
-    if (prefile) {
-      return prefile;
-    }
 
     // If there's no matching prefile try the pilots.
-    const pilot = vatsimData.pilots.find(
-      (pilot) => pilot.callsign === callsign,
-    );
+    result ??= vatsimData.pilots.find((pilot) => pilot.callsign === callsign);
 
-    if (pilot) {
-      return pilot;
-    }
-
-    // Nothing found, return undefined.
-    return;
+    return result;
   } catch (error) {
     console.error("Unable to get Vatsim flight plans:", error);
     return;
   }
+}
 
-  return;
+export function flightPlanToScenario(flightPlan: VatsimFlightPlan) {
+  if (!flightPlan?.callsign) {
+    console.error("VATSIM flight plans must have a callsign.");
+    throw new Error("VATSIM flight plans must have a callsign");
+  }
+
+  const scenario: Scenario = {
+    plan: {
+      aid: flightPlan.callsign,
+      alt: convertToNumber(flightPlan.flight_plan?.altitude),
+      bcn:
+        convertToNumber(flightPlan.flight_plan?.assigned_transponder) ??
+        getRandomExternalBcn(),
+      cid: getRandomCid(),
+      dep: flightPlan.flight_plan?.departure,
+      dest: flightPlan.flight_plan?.arrival,
+      pilotName: getRandomName(),
+      rmk: flightPlan.flight_plan?.remarks,
+      rte: flightPlan.flight_plan?.route,
+      spd: convertToNumber(flightPlan.flight_plan?.cruise_tas),
+      //      typ: flightPlan.equipmentType,
+      //eq: flightPlan.equipmentSuffix,
+      homeAirport: getRandomAirportCode(),
+      vatsimId: getRandomVatsimId(),
+    },
+    airportConditions: {},
+    isValid: true,
+    isDraft: true,
+    hasAudio: false,
+    explanations: [],
+  };
+
+  return scenario;
 }
